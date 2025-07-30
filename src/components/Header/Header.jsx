df9a8cc3fafc404828a39a344f887c906d01ae0e
RaxmonovaAziza <raxmonovaa59@gmail.com>
2025-07-30 18:09:41 +0500

    gg

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className='header__container'>
+            <ul className='header__list'>
+                <li className='header__item'><a className='header__link' href="">home</a></li>
+                 <li className='header__item'><a className='header__link' href="">about</a></li>
+                  <li className='header__item'><a className='header__link' href="">contact</a></li>
+                    <li className='header__item'><a className='header__link' href="">contact</a></li>
+            </ul>
+
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
